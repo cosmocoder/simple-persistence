b32080276e9eb9ac9eea21526096353e65301b08
Rodrigo Mallmann Guerra <rodmguerra@gmail.com>
2012-10-09 18:41:26 -0300

    Unbinding model

--- src/main/java/org/simplepersistence/model/Entity.java
+++ src/main/java/org/simplepersistence/model/Entity.java
@@ -1,29 +1,51 @@
 package org.simplepersistence.model;
 
-import com.google.common.base.Objects;
+import com.google.common.base.*;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import org.simplepersistence.model.types.EntityType;
+import org.simplepersistence.model.types.PropertyType;
 
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
+import static com.google.common.base.Predicates.*;
+import static com.google.common.collect.Collections2.transform;
 import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Maps.*;
+import static com.google.common.collect.Sets.filter;
 import static com.google.common.collect.Sets.newHashSet;
 
 public class Entity {
     private final EntityType type;
-    private final Set<Property> identifierProperties = newHashSet();
+    private Set<Property> identifierProperties;
     private final Map<String,Attribute> attributes;
+    private final Map<String,AssociationMember> associations;
 
-    public Entity(final EntityType type,
-                  final Set<Property> identifier,
-                  final Map<String, Attribute> attributes) {
+    public Entity(final EntityType type, Map<String,Property> properties) {
         this.type = type;
-        this.attributes = attributes;
+        this.attributes = filterProperties(properties, Attribute.class);
+        this.associations = filterProperties(properties, AssociationMember.class);
+        this.identifierProperties = newHashSet(Iterables.filter(properties.values(), compose(Predicates.in(type.getIdentifier()), getTypeFunction())));
     }
 
-    void addIdentifierPropety(Property property) {
-        this.identifierProperties.add(property);
+    private static Function<Property, PropertyType> getTypeFunction() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private <T extends Property> Map<String, T> filterProperties(final Map<String, Property> properties, final Class<T> clazz) {
+        return (Map<String,T>) filterValues(properties, instanceOf(clazz));
+    }
+
+    private Entity(final EntityType type,
+                  final Set<Property> identifierProperties,
+                  final Map<String, Attribute> attributes,
+                  final Map<String, AssociationMember> associations) {
+        this.type = type;
+        this.attributes = attributes;
+        this.associations = associations;
     }
 
     public EntityType getType() {
